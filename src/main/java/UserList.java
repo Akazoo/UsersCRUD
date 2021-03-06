56301c97c750442de2abab1ae298dd16eb966870
akazoo <pawel.kucharski1996@interia.pl>
2021-03-06 14:46:06 +0100

    dziala

---
+++ src/main/java/UserList.java
@@ -0,0 +1,34 @@
+import utils.User;
+import utils.UserDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/list")
+public class UserList extends HttpServlet {
+
+    protected void doPost(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        UserDAO.createUserTable();
+
+        UserDAO userDAO = new UserDAO();
+        request.setAttribute("users", userDAO.findAll());
+        getServletContext().getRequestDispatcher("/users/list.jsp")
+                .forward(request, response);
+
+    }
+}
